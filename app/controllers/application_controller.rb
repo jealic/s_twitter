ac63ef5f385da739a19e9d44dea6f8733037aaa6
Chiehyun <jealic@gmail.com>
2018-06-20 00:14:50 +0800

    setup admin authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "You're not the administrator!"
+      redirect_to root_path
+    end
+  end
+  
 end
