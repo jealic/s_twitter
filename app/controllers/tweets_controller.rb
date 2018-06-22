cbbb5d1b62a2c632ca2750bc0d6f12eecc5ee349
Chiehyun <jealic@gmail.com>
2018-06-23 02:24:19 +0800

    add front end index for tweet as well as its controller. next: 解決關連方法

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,26 @@ class TweetsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    
+    @tweets = Tweet.all
+    @tweet = Tweet.new
+  end
+  
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      flash[:notice] = "You've updated with a tweet."
+      redirect_to tweets_path
+    else
+      @tweets = Tweet.all
+      render :index
+    end
+  end
+
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description, :user_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "tweets#index"
+  resources :tweets, only: [:index, :create]
 
   namespace :admin do
     root "tweets#index"
